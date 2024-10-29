4e1af0c2d89c24b50f8c0f4adc48e33f904d79ed
Andrea Manuel Simón <91722324+andmansim@users.noreply.github.com>
2024-10-29 18:07:06 +0100

    datos

---
+++ recoger_datos.py
@@ -0,0 +1,21 @@
+#Installar yfinance
+
+import yfinance as yf
+import pandas as pd
+
+#Definir el símbolo de IBEX 35
+symbol = "^IBEX"
+
+#Descargar datos históricos desde 2012-01-01 hasta hoy
+data = yf.download(symbol, start="2012-01-01", end=pd.Timestamp.today())
+
+#Mostrar los primeros registros
+print(data.head())
+
+#Guardar los datos en un archivo CSV
+data.to_csv("ibex35_historico.csv", sep=';', decimal= ',', encoding='utf-8')
+
+#Ajustar el date time para que sea el formato solo YYYY-MM-DD
+#Error cuadratico medio, coef de correlación y ver la predicción
+#Mirar el formato de los datos y si cuadra con los de la web yahoo finance IBEX 35
+#Establecer bien las columnas para que tengan sentido, cambiarles el nombre y hacerlo mejor. 
