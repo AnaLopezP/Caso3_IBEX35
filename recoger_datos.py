01e79fab7460f9bfcc5cab63269a92b006ad7c70
Andrea Manuel Simón <91722324+andmansim@users.noreply.github.com>
2024-10-29 18:07:30 +0100

    ccc

--- recoger_datos.py
+++ recoger_datos.py
@@ -15,7 +15,7 @@ print(data.head())
 #Guardar los datos en un archivo CSV
 data.to_csv("ibex35_historico.csv", sep=';', decimal= ',', encoding='utf-8')
 
-#Ajustar el date time para que sea el formato solo YYYY-MM-DD
+#Ajustar el date time para que sea el formato solo YYYY-MM-DD, pasar todos los datos numericos a int o float
 #Error cuadratico medio, coef de correlación y ver la predicción
 #Mirar el formato de los datos y si cuadra con los de la web yahoo finance IBEX 35
 #Establecer bien las columnas para que tengan sentido, cambiarles el nombre y hacerlo mejor. 
