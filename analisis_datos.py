c72e5230754c3bc6cbd4f0c13bf3e15dcecfb891
Andrea Manuel Simón <91722324+andmansim@users.noreply.github.com>
2024-11-04 16:13:22 +0100

    jdnfjdnfs

--- analisis_datos.py
+++ analisis_datos.py
@@ -10,14 +10,26 @@ df = pd.read_csv("ibex35_historico_limpio.csv", sep=';', decimal=',', encoding='
 
 decomposition = seasonal_decompose(df['Cierre'], model='additive', period=90)
 
-# Graficar los componentes para cada columna
-fig, axes = plt.subplots(4, 1, sharex=True, figsize=(10, 8))
+# Graficar los componentes (Observado, Tendencia)
+fig, axes = plt.subplots(2, 1, sharex=True, figsize=(10, 5))
 fig.suptitle(f'Descomposición de la Serie Temporal: Cierre')
 
 decomposition.observed.plot(ax=axes[0], title='Observado')
 decomposition.trend.plot(ax=axes[1], title='Tendencia')
-decomposition.seasonal.plot(ax=axes[2], title='Estacionalidad')
-decomposition.resid.plot(ax=axes[3], title='Residuo')
 
 plt.tight_layout(rect=[0, 0, 1, 0.96])
 plt.show()
+
+# Graficar los componentes (Estacionalidad, Residuo)
+fig1, axes = plt.subplots(2, 1, sharex=True, figsize=(10, 5))
+fig1.suptitle(f'Descomposición de la Serie Temporal: Cierre')
+
+decomposition.seasonal.plot(ax=axes[0], title='Estacionalidad')
+decomposition.resid.plot(ax=axes[1], title='Residuo')
+
+plt.tight_layout(rect=[0, 0, 1, 0.96])
+plt.show()
+
+#Guardamos las gráficas
+fig.savefig('ibex35_hist_tend_observ.png')
+fig1.savefig('ibex35_hist_estacional_residuo.png')
