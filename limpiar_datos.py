5fbeedeaecbbe931045bd3d09c8b686739a7f093
Ana López <anamuyamiga@gmail.com>
2024-11-04 15:45:07 +0100

    jjje

--- limpiar_datos.py
+++ limpiar_datos.py
@@ -39,10 +39,7 @@ print(df.dtypes)
 df = df.round(2)
 print(df.head())
 
-
-
-
-#Ajustar el date time para que sea el formato solo YYYY-MM-DD, pasar todos los datos numericos a int o float
+#Ajustar el date time para que sea el formato solo YYYY-MM-DD, pasar todos los datos numericos a int o float HECHO
 #Error cuadratico medio, coef de correlación y ver la predicción
-#Mirar el formato de los datos y si cuadra con los de la web yahoo finance IBEX 35
-#Establecer bien las columnas para que tengan sentido, cambiarles el nombre y hacerlo mejor. 
+#Mirar el formato de los datos y si cuadra con los de la web yahoo finance IBEX 35 HECHO
+#Establecer bien las columnas para que tengan sentido, cambiarles el nombre y hacerlo mejor. HECHO
