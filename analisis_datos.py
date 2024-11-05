af948c0434958b2b77af334200546a83ba8275f9
Andrea Manuel Simón <91722324+andmansim@users.noreply.github.com>
2024-11-05 18:00:44 +0100

    esta confuso

--- analisis_datos.py
+++ analisis_datos.py
@@ -7,32 +7,33 @@ from statsmodels.tsa.seasonal import seasonal_decompose
 df = pd.read_csv("ibex35_historico_limpio.csv", sep=';', decimal=',', encoding='utf-8', index_col=0)
 
 # Aplicar la descomposición a cada columna
+def descomposicion (df):
+    decomposition = seasonal_decompose(df['Cierre'], model='additive', period=365)
 
-decomposition = seasonal_decompose(df['Cierre'], model='additive', period=365)
+    # Graficar los componentes (Observado, Tendencia)
+    fig, axes = plt.subplots(2, 1, sharex=True, figsize=(10, 5))
+    fig.suptitle(f'Descomposición de la Serie Temporal: Cierre')
 
-# Graficar los componentes (Observado, Tendencia)
-fig, axes = plt.subplots(2, 1, sharex=True, figsize=(10, 5))
-fig.suptitle(f'Descomposición de la Serie Temporal: Cierre')
+    decomposition.observed.plot(ax=axes[0], title='Observado')
+    decomposition.trend.plot(ax=axes[1], title='Tendencia')
 
-decomposition.observed.plot(ax=axes[0], title='Observado')
-decomposition.trend.plot(ax=axes[1], title='Tendencia')
+    plt.tight_layout(rect=[0, 0, 1, 0.96])
+    plt.show()
 
-plt.tight_layout(rect=[0, 0, 1, 0.96])
-plt.show()
+    # Graficar los componentes (Estacionalidad, Residuo)
+    fig1, axes = plt.subplots(2, 1, sharex=True, figsize=(10, 5))
+    fig1.suptitle(f'Descomposición de la Serie Temporal: Cierre')
 
-# Graficar los componentes (Estacionalidad, Residuo)
-fig1, axes = plt.subplots(2, 1, sharex=True, figsize=(10, 5))
-fig1.suptitle(f'Descomposición de la Serie Temporal: Cierre')
+    decomposition.seasonal.plot(ax=axes[0], title='Estacionalidad')
+    decomposition.resid.plot(ax=axes[1], title='Residuo')
 
-decomposition.seasonal.plot(ax=axes[0], title='Estacionalidad')
-decomposition.resid.plot(ax=axes[1], title='Residuo')
+    plt.tight_layout(rect=[0, 0, 1, 0.96])
+    plt.show()
 
-plt.tight_layout(rect=[0, 0, 1, 0.96])
-plt.show()
-
-#Guardamos las gráficas
-fig.savefig('imgs/ibex35_hist_tend_observ.png')
-fig1.savefig('imgs/ibex35_hist_estacional_residuo.png')
+    #Guardamos las gráficas
+    fig.savefig('imgs/ibex35_hist_tend_observ.png')
+    fig1.savefig('imgs/ibex35_hist_estacional_residuo.png')
+    return decomposition
 
 #Ver lógica de la curva de tendencia y estacionalidad
 '''
@@ -47,18 +48,7 @@ Dividir por fechas, ej: 2023-2024 y ver así más en concreto
 
 '''
 
-#Ver maximos y minimos
 
-# Sacamos las filas que tienen un cierre alrededor de 8000
-tendencia = decomposition.trend
-print(tendencia[tendencia < 8000])
-
-
-# saco las fechas de todas las filas entre estos valores
-a = tendencia[tendencia < 8000].index
-for i in a:
-    print(i)
-# print(df[ 7500 > df['Cierre'] > 8500])
 
 
 # Prueba de Dickey-Fuller Aumentada (ADF) 
@@ -81,14 +71,33 @@ def adf (df):
 
 import pymannkendall as mk
 
-result = mk.original_test(df['Cierre'])
-print(result)
+def ver_tendencia (df):
+    result = mk.original_test(df['Cierre'])
+    print(result)
 
-if result.p < 0.05:
-    # La dirección de la tendencia se determina mediante el signo de la estadística
-    if result.trend == 'increasing':
-        print("Existe una tendencia creciente significativa.")
+    if result.p < 0.05:
+        # La dirección de la tendencia se determina mediante el signo de la estadística
+        if result.trend == 'increasing':
+            print("Existe una tendencia creciente significativa.")
+        else:
+            print("Existe una tendencia decreciente significativa.")
     else:
-        print("Existe una tendencia decreciente significativa.")
-else:
-    print("No se detecta una tendencia significativa.")
+        print("No se detecta una tendencia significativa.")
+    
+    
+if __name__ == '__main__':
+    descomposicion(df)
+    decomposition = adf(df)
+    #Ver maximos y minimos
+
+    # Sacamos las filas que tienen un cierre alrededor de 8000
+    tendencia = decomposition.trend
+    print(tendencia[tendencia < 8000])
+
+
+    # saco las fechas de todas las filas entre estos valores
+    a = tendencia[tendencia < 8000].index
+    for i in a:
+        print(i)
+    # print(df[ 7500 > df['Cierre'] > 8500])
+    ver_tendencia(df)
